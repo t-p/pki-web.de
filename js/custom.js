e29c9af080d9e4d17c67aae1a670e53bf9fb469b
Thomas Pfeiffer <email@pfeiffer.pw>
2013-12-26 21:30:56 +0100

    add new cabins for 2013

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,5 @@ WebFont.load({
 
 $(document).ready(function() {
 
-    $("a[rel=cabin_group]").fancybox({
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none',
-		'titlePosition' 	: 'over',
-		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
-			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-			}
-	});
+  $("a[rel=cabin_group]").fancybox();
 });
